804c5a0ceff173f7f76e6961b30ac5b4fb3296ae
Just2Deep <deepakrajsrs@gmail.com>
2022-08-14 01:00:09 +0530

    git push

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+"use strict";
+
+// navbar variables
+
+const nav = document.querySelector(".mobile-nav");
+const navMenuBtn = document.querySelector(".nav-menu-btn");
+const navCloseBtn = document.querySelector(".nav-close-btn");
+
+// navToggle functions
+const navToggleFunc = function () {
+  nav.classList.toggle("active");
+};
+
+navMenuBtn.addEventListener("click", navToggleFunc);
+navCloseBtn.addEventListener("click", navToggleFunc);
+
+//theme toggle variables
+const themeBtn = document.querySelectorAll(".theme-btn");
+
+for (let i = 0; i < themeBtn.length; i++) {
+  themeBtn[i].addEventListener("click", function () {
+    //toggle light and dark theme class from body
+    //when clicked 'theme-btn'
+
+    document.body.classList.toggle("light-theme");
+    document.body.classList.toggle("dark-theme");
+
+    for (let i = 0; i < themeBtn.length; i++) {
+      //when the theme-btn is clicked
+      //it toggles classes between light and dark for all theme-btn.
+      themeBtn[i].classList.toggle("light");
+      themeBtn[i].classList.toggle("dark");
+    }
+  });
+}
